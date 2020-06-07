e07822f05526f10d8e67bd9e3a4f278edd2d2c5f
Inheritance <dideful@mail.ru>
2020-06-08 04:13:29 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/controllers/BotController.java

--- src/main/java/controllers/BotController.java
+++ src/main/java/controllers/BotController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import domain.AccessToken;
 import domain.UserLoginData;
+import domain.models.Student;
 import domain.models.User;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
@@ -29,7 +30,8 @@ public class BotController extends TelegramLongPollingBot {
     private final String USERNAME = "DimkeksBot";
 
     private UserLoginData user = new UserLoginData();
-    private User currentUser = null;
+    private User currentUser = new User();
+    private boolean isAuthorised=false;
 
     SendMessage sendMessage = new SendMessage();
     static int cnt = 0;
@@ -46,15 +48,21 @@ public class BotController extends TelegramLongPollingBot {
         Integer messageId = receivedMessage.getMessageId();
 
         if (receivedMessage.getText().equals("/start")) {
-            sendMsg(sendMessage.setText("If you don't know how to use please go to /help"));
+            sendMsg(sendMessage.setText("/help - all needed commands"));
         }
 
         if (receivedMessage.getText().equals("/help")) {
-            sendMsg(sendMessage.setText("To sign in you have to confirm your login and password \n commands:\n" +
-                    "/set_username <<your login>>\n" +
-                    "/set_password <<your password>>\n\n\n" +
-                    "press /login after settings\n" +
-                    "press /data is you wanna see your data"));
+            if (isAuthorised==false) {
+                sendMsg(sendMessage.setText("To sign in you have to confirm your login and password \n commands:\n" +
+                        "/set_username <<your login>>\n" +
+                        "/set_password <<your password>>\n\n\n" +
+                        "press /login after settings to sign in\n"));
+            } else {
+                sendMsg(sendMessage.setText("Commands:\n" +
+                        "/mygroup - show's your group\n" +
+                        "/myfaculty - show's your faculty\n" +
+                        "/myschedule - give's you schedule\n"));
+            }
         }
 
         if (receivedMessage.getText().startsWith("/set_username")) {
@@ -67,7 +75,7 @@ public class BotController extends TelegramLongPollingBot {
                     sendMsg(sendMessage.setText("Please enter non-empty username"));
                 } else {
                     user.setUsername(takenmsg[1]);
-                    sendMsg(sendMessage.setText("Username was successfully updated"));
+                    sendMsg(sendMessage.setText("Username has been established"));
                 }
             }
         }
@@ -76,16 +84,15 @@ public class BotController extends TelegramLongPollingBot {
         if (receivedMessage.getText().startsWith("/set_password")) {
             String text = receivedMessage.getText();
             if (text.length() == 13) {
-                sendMsg(sendMessage.setText("Please enter non-empty username"));
+                sendMsg(sendMessage.setText("Please enter non-empty password"));
             } else {
                 String[] takenmsg = text.split(" ");
                 if (takenmsg[1].isEmpty()) {
-                    sendMsg(sendMessage.setText("Please enter non-empty username"));
+                    sendMsg(sendMessage.setText("Please enter non-empty password"));
                 } else {
                     user.setPassword(takenmsg[1]);
-                    sendMsg(sendMessage.setText("Password was successfully updated"));
+                    sendMsg(sendMessage.setText("Password has been established"));
                     sendMsg(sendMessage.setText(deleteMsg(chatId, messageId)));
-                    sendMsg(sendMessage.setText("Your password was hide"));
                 }
             }
         }
@@ -94,33 +101,42 @@ public class BotController extends TelegramLongPollingBot {
             UserLoginData userLoginData = new UserLoginData();
             userLoginData.setUsername(user.getUsername());
             userLoginData.setPassword(user.getPassword());
+            User studentData = userService.getUserByUsername(user.getUsername());
             if (login(userLoginData)) {
-                sendMsg(sendMessage.setText("Successful login"));
+                sendMsg(sendMessage.setText("Welcome " + studentData.getName() + " " + studentData.getSurname()));
+                currentUser.setUsername(user.getUsername());
+                currentUser.setPassword(user.getPassword());
+                isAuthorised=true;
             } else {
                 sendMsg(sendMessage.setText("Incorrect username or password!"));
             }
         }
 
-        if (receivedMessage.getText().equals("/data")) {
-            if (currentUser == null) {
-                sendMsg(sendMessage.setText("Firstly SignUp please"));
-            } else {
-                sendMsg(sendMessage.setText(currentUser.toString()));
-            }
+        if (receivedMessage.getText().equals("/myuserdata")) {
+            sendMessage.setText(userService.getUserByUsername(user.getUsername()).toString());
         }
+        if (receivedMessage.getText().equals("/mygroup")) {
+            sendMessage.setText("HELLLOO!");
+            User student = userService.getStudentDataByUsername(currentUser);
+        }
+        if (receivedMessage.getText().equals("/myfaculty")) {
+            User student = userService.getStudentDataByUsername(currentUser);
+            sendMessage.setText(((Student) student).getOwnFaculty());
+        }
+
 
         if (receivedMessage.getText().equals("/mySchedule")) {
             if (currentUser == null) {
                 sendMsg(sendMessage.setText("Firstly SignUp please"));
             } else if (currentUser.getRole() != "Teacher") {
-                String path = "C:\\Users\\magzhan\\Desktop\\schedules\\schedules\\";
-                path += currentUser.getEducationProgramm() + " " + currentUser.getGroup() + " " + ".xlsx";
-                File file = new File();
+                String path = "C:\\Users\\didef\\IdeaProjects\\telegram_bot\\src\\main\\java\\DataBase\\schedules\\";
+                User studentData = userService.getStudentDataByUsername(currentUser);
+                path += ((Student) studentData).getOwnClass() + " " + ((Student) studentData).getOwnFaculty() + " " + ".xlsx";
                 SendDocument sendDocumentRequest = new SendDocument();
                 sendDocumentRequest.setChatId(receivedMessage.getChatId());
-                sendDocumentRequest.setDocument();
+                sendDocumentRequest.setDocument(path);
                 sendDocumentRequest.setCaption("This is your schedule");
-                sendDocument(sendDocumentRequest);
+                sendDocumentRequest.setDocument(path);
             }
         }
     }
@@ -147,15 +163,6 @@ public class BotController extends TelegramLongPollingBot {
         } catch (Exception e) {
             return false;
         }
-    }
-
-    private void sendDocUploadingAFile(Long chatId, java.io.File save,String caption) throws TelegramApiException {
-
-        SendDocument sendDocumentRequest = new SendDocument();
-        sendDocumentRequest.setChatId(chatId);
-        sendDocumentRequest.setNewDocument(save);
-        sendDocumentRequest.setCaption(caption);
-        sendDocument(sendDocumentRequest);
     }
 
     public void sendMsg(SendMessage sendMessage) {
