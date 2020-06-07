418838da9dc93b09d62578fb09dd4ff26599c0b8
Inheritance <dideful@mail.ru>
2020-06-07 18:32:57 +0600

    Dimash Postgre

--- src/main/java/controllers/BotController.java
+++ src/main/java/controllers/BotController.java
@@ -28,33 +28,77 @@ public class BotController extends TelegramLongPollingBot {
     private final String TOKEN = "1251502840:AAFNkleaB9Bs3DkwXraO1lfUifcZN5MTv8Q";
     private final String USERNAME = "aitubotit1908";
 
+    private String username;
+    private String password;
+    private boolean currentUser=false;
+
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message receivedMessage = update.getMessage();
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(receivedMessage.getChatId());
+        String name = receivedMessage.getFrom().getFirstName();
+        String surname = receivedMessage.getFrom().getLastName();
+        surname = (surname != null ? surname : "");
+
+        if(currentUser==false) {
+            if (receivedMessage.getText().equals("/start")) {
+                sendMessage.setText("Sign in:" + "\n" + "username:<<your username>>");
+            }
+            if (receivedMessage.getText().startsWith("username:")) {
+                String[] takenmsg = receivedMessage.getText().split(":");
+                username = takenmsg[1];
+                sendMessage.setText("Write your password:" + "\n" + "password:<<your password>>");
+            }
+            if(username!=null) {
+                if (receivedMessage.getText().startsWith("password:")) {
+                    String[] takenmsg = receivedMessage.getText().split(":");
+                    password = takenmsg[1];
+                    UserLoginData userLoginData = new UserLoginData();
+                    userLoginData.setUsername(username);
+                    userLoginData.setPassword(password);
+                    if (login(userLoginData)) {
+                        sendMessage.setText("Congrats!");
+                    } else {
+                        sendMessage.setText("NotCongrats!");
+                    }
+
+                }
+            }
+        }
+        else {
+            SendMessage message = new SendMessage();
+            if(receivedMessage.getText().equals("/myuserdata")){
+                message.setText(userService.getUserByUsername(username).toString());
+            }
+        }
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public boolean login(UserLoginData data) {
         try {
             AccessToken token = authService.authenticateUser(data);
+            currentUser=true;
             return true;
         } catch (Exception e) {
+            currentUser=false;
             return false;
         }
-    }
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        Message receivedMessage = update.getMessage();
-        String inputText = update.getMessage().getText();
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(receivedMessage.getChatId());
-
-        String name = receivedMessage.getFrom().getFirstName();
-        name = (name != null ? name : "");
-        String surname = receivedMessage.getFrom().getLastName();
-        surname = (surname != null ? surname : "");
-        String userInfo = name + " " + surname;
-
-
-    
     }
 
     @Override
--- src/main/java/repositories/PostgresRepository.java
+++ src/main/java/repositories/PostgresRepository.java
@@ -10,8 +10,8 @@ public class PostgresRepository implements IDBRepository {
     @Override
     public Connection getConnection() {
         try {
-            String conStr = "jdbc:postgresql://localhost:5432/myapp";
-            Connection con = DriverManager.getConnection(conStr, "postgres", "123");
+            String conStr = "jdbc:postgresql://localhost:5432/TelegramBotDb";
+            Connection con = DriverManager.getConnection(conStr, "postgres", "lbvfi2281");
             return con;
         } catch (SQLException ex) {
             ex.printStackTrace();
